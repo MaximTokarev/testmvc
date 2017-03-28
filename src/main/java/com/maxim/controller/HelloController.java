74f880c2d6491e7ac1534a7b79fe33ad93e064f3
Maksym Tokariev <maksym_tokariev@epam.com>
2017-03-28 22:05:29 +0300

    xzx

---
+++ src/main/java/com/maxim/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.maxim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/greeting")
+    public String sayHello(Model model) {
+        model.addAttribute("greeting","Hello World");
+        System.out.println("dasd");
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Main Page</title>
+</head>
+<body>
+<h1>${greeting}</h1>
+</body>
+</html>
